a5e5fd46e80cccf4632e1cb7a6648bcf7c45e66d
Kevin Valdebenito <ke.valdebenitol@duocuc.cl>
2026-09-09 01:28:47 -0300

    KOT-003 - Agregar motor de tarifas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,17 @@ val nombreSistema: String = "GameZone"
 val capacidad: Int = 10
 var recaudacionTotal: Double = 0.0
 
+
+val minutoUso: Int = 75
+val tarifaHora: Double = 800.0
+
+val costoBase = calcularCostoBase(
+    minutoUso,
+    tarifaHora
+)
+
+val totalConIva = aplicarIva(costoBase)
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
@@ -12,6 +23,21 @@ fun main() {
     println("Capacidad: $capacidad")
     println("Recaudaciad: $recaudacionTotal")
 
+
     recaudacionTotal = 800.0
     println("Nuevo recaudaciado: $recaudacionTotal")
+
+    println("Conto Base: $costoBase")
+    println("Total Con Iva: $totalConIva")
+}
+
+fun calcularCostoBase(
+    minutos: Int,
+    tarifaHora: Double
+): Double {
+    return (minutos / 60.0)*tarifaHora
+}
+
+fun aplicarIva(monto: Double): Double {
+    return monto * 1.19
 }
